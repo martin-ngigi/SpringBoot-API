06c7256b511e62209406e2559086b11b5dec4a78
martin-ngigi <martinwainaina001@gmail.com>
2022-12-25 17:32:25 +0300

    Added a route example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -13,4 +15,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	//http://localhost:8080/hello
+	@GetMapping("/hello")
+	public String hello(){
+		return  "Hello world";
+	}
+
+
+	//http://localhost:8080/hello-list
+	@GetMapping("/hello-list")
+	public List<String> helloList(){
+		return  List.of("Martin", "Wainaina");
+	}
 }
