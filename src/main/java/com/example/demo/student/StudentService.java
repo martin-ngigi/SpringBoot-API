7f71b282596b93f18cdfa34fc6616267276b09a1
martin-ngigi <martinwainaina001@gmail.com>
2022-12-26 16:07:51 +0300

    Added student repository for the data access layer

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,6 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +9,24 @@ import java.util.List;
 
 @Service //Will make it a spring bin component/service.
 public class StudentService {
+
+    private final  StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return  List.of(
-                new Student(
-                        1L,
-                        "Martin",
-                        "martinwainaina001@gmail.com",
-                        LocalDate.of(1999, Month.NOVEMBER, 23),
-                        23
-                )
-        );
+//        return  List.of(
+//                new Student(
+//                        1L,
+//                        "Martin",
+//                        "martinwainaina001@gmail.com",
+//                        LocalDate.of(1999, Month.NOVEMBER, 23),
+//                        23
+//                )
+//        );
+        return  studentRepository.findAll();
     }
 }
